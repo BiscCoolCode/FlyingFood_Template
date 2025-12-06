135c2aa5b21623f9e7111a47d1494a41ea06fd7e
BiscCoolCode <bisc@coolcode.io>
2025-12-06 12:11:18 +0100

    Stadt + Kaugummi + Schiessen

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class FoodCannon : MonoBehaviour
 {
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject chewingGum;
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(chewingGum, shootPoint.position, Random.rotation);
+        }
+    }
 }
