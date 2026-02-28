4229d5ff44a92835026ef1f011b98951ea7d8613
BiscCoolCode <bisc@coolcode.io>
2026-02-28 10:09:58 +0100

    Gums Dialog erweitert

--- Assets/Scripts/GumsDialog.cs
+++ Assets/Scripts/GumsDialog.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 using TMPro;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using UnityEngine.UI;
 
 public class GumsDialog : MonoBehaviour
 {
     [TextAreaAttribute][SerializeField] private string[] _Dialog;
+    [SerializeField] private Button _OkButton;
 
     private TMP_Text _textbox;
+    private int _dialogNumber;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _textbox = GetComponent<TMP_Text>();
-        _textbox.maxVisibleCharacters = 0;
-        PrintText();
+        ChangeText();
+        _OkButton.onClick.AddListener(ChangeText);
     }
 
     // Update is called once per frame
@@ -25,11 +28,22 @@ public class GumsDialog : MonoBehaviour
 
     private async void PrintText()
     {
-        while(_textbox.maxVisibleCharacters < _textbox.text.Length)
+        while (_textbox.maxVisibleCharacters < _textbox.text.Length)
         {
             _textbox.maxVisibleCharacters++;
             await Task.Delay(50);
         }
+
+        _OkButton.gameObject.SetActive(true);
         
+    }
+
+    private void ChangeText()
+    {
+        _textbox.text = _Dialog[_dialogNumber];
+        _OkButton.gameObject.SetActive(false);
+        _textbox.maxVisibleCharacters = 0;
+        PrintText();
+        _dialogNumber++;
     }
 }
