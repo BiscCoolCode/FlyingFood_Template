ee2196771d89ab2672f777808444f0a69f3d6f1e
BiscCoolCode <bisc@coolcode.io>
2026-03-28 12:07:36 +0100

    Start Menu-State programmmiert, maus-Problem wieder da

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private ExamplePlayer _Player;
     [SerializeField] private GameObject _Foodcanon;
+    [SerializeField] private Camera _MenuCamera;
+    [SerializeField] private GameObject _MenuUI;
+    [SerializeField] private GameObject _GameUI;
+    [SerializeField] private GameObject _Crosshair;
 
     public static GameManager Instance { get; private set; }
     public GameState CurrentState {  get; private set; }
@@ -25,7 +29,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ChangeState(GameState.Intro);
+        ChangeState(GameState.StartMenu);
     }
 
 
@@ -33,6 +37,12 @@ public class GameManager : MonoBehaviour
     {
         switch (gameState)
         {
+            case GameState.StartMenu:
+                CurrentState = GameState.StartMenu;
+                EnableMenu(true);
+                Cursor.lockState = CursorLockMode.None;
+                break;
+
             case GameState.Intro:
                 CurrentState = GameState.Intro;
                 Time.timeScale = 0;
@@ -40,6 +50,7 @@ public class GameManager : MonoBehaviour
                 _Player.CharacterCamera.enabled = false;
                 _Foodcanon.SetActive(false);
                 Cursor.lockState = CursorLockMode.None;
+                EnableMenu(false);
                 break;
 
             case GameState.Play:
@@ -49,7 +60,33 @@ public class GameManager : MonoBehaviour
                 _Player.CharacterCamera.enabled = true;
                 _Foodcanon.SetActive(true);
                 Cursor.lockState = CursorLockMode.Locked;
+                EnableMenu(false);
                 break;
         }
+    }
+
+    private void EnableMenu(bool enabled)
+    {
+        if (enabled)
+        { 
+            _Player.enabled = false;
+            _GameUI.SetActive(false);
+            _MenuCamera.enabled = true;
+            _MenuUI.SetActive(true);
+            _Crosshair.SetActive(false);
+        }
+        else
+        {
+            _Player.enabled = true;
+            _GameUI.SetActive(true);
+            _MenuCamera.enabled = false;
+            _MenuUI.SetActive(false);
+            _Crosshair.SetActive(true);
+        }
+    }
+
+    public void StartGame()
+    {
+        ChangeState(GameState.Intro);
     }
 }
